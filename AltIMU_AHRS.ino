add9397771db38fbebf3cf2bdeee89a7873d8ab7
S. James Remington <jremington@users.noreply.github.com>
2020-12-17 09:17:38 -0800

    Update AltIMU_AHRS.ino
    12/17/2020 cleaned up comments and remove dead code

--- AltIMU_AHRS.ino
+++ AltIMU_AHRS.ino
@@ -1,17 +1,14 @@
 //
-// AltIMU-10 v3 Magwick/Mahony AHRS  S.J. Remington 4/2020
+// AltIMU-10 v3 Magwick/Mahony AHRS  S.J. Remington 
+// last update 12/17/2020, clean up comments
 
-// VERY VERY IMPORTANT!
+// Standard sensor orientation: Z Up X North Y West
+
 // Both the accelerometer and magnetometer MUST be properly calibrated for this program to work, and the gyro offset must be determned.
 // Follow the procedure described in http://sailboatinstruments.blogspot.com/2011/08/improved-magnetometer-calibration.html
 // or in more detail, the tutorial https://thecavepearlproject.org/2015/05/22/calibrating-any-compass-or-accelerometer-for-arduino/
-//
 // To collect data for calibration, use the companion programs altimu10v3_cal and Magneto 1.2 from sailboatinstruments.blogspot.com
 //
-//
-
-//The L3GD20H default full scale setting is +/- 245 dps.
-//for a conversion factor of 8.75 mdps/LSB (least significant bit)
 
 #include <Wire.h>
 #include <LSM303.h>
@@ -19,8 +16,6 @@
 
 L3G gyro;
 LSM303 compass;
-
-// NOTE:  Serial.begin(230400);
 
 // vvvvvvvvvvvvvvvvvv  VERY VERY IMPORTANT vvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 //These are the previously determined offsets (bias) and scale factors for accelerometer and magnetometer,
@@ -41,6 +36,9 @@ float A_Ainv[3][3]
   {  0.00382, -0.00098,  0.59250}
 };
 
+//The L3GD20H default full scale setting is +/- 245 dps.
+#define gscale 8.75e-3*(PI/180.0)  //gyro default 8.75 mdps -> rad/s
+
 float G_off[3] = {83.7, -293.8, -380.5}; //raw offsets, determined for gyro at rest
 // ^^^^^^^^^^^^^^^^^^^ VERY VERY IMPORTANT ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
@@ -56,7 +54,7 @@ unsigned long print_ms = 500; //print orientation angles every "print_ms" millis
 float Axyz[3];
 float Gxyz[3];
 float Mxyz[3];
-#define gscale 8.75e-3*(PI/180.0)  //gyro default 8.75 mdps -> rad/s
+
 
 // NOW USING MAHONY FILTER
 
@@ -66,32 +64,12 @@ float Mxyz[3];
 #define Kp 30.0
 #define Ki 0.0
 
-// Madwick filter parameters
-static float GyroMeasError = 40.0 * (PI / 180.0);
-// gyroscope measurement drift in rad/s/s (start at 0.0 deg/s/s)
-static float GyroMeasDrift = 0.0 * (PI / 180.0);
-
-// There is a tradeoff in the beta parameter between accuracy and response
-// speed. In the original Madgwick study, beta of 0.041 (corresponding to
-// GyroMeasError of 2.7 degrees/s) was found to give optimal accuracy.
-// However, with this value, the LSM9SD0 response time is about 10 seconds
-// to a stable initial quaternion. By increasing beta by factor of
-// fifteen, the response time constant is reduced to ~2 sec. I haven't
-// noticed any reduction in solution accuracy.
-static float beta = sqrt(3.0f / 4.0f) * GyroMeasError;   // Compute beta
-// Compute zeta, the other free parameter in the Madgwick scheme usually
-// set to a small or zero value
-static float zeta = sqrt(3.0f / 4.0f) * GyroMeasDrift;
-
-
-// Vector to hold integral error for Mahony method
-static float eInt[3] = {0.0, 0.0, 0.0};
 // Vector to hold quaternion
 static float q[4] = {1.0, 0.0, 0.0, 0.0};
 static float yaw, pitch, roll; //Euler angle output
 
 void setup() {
-  Serial.begin(230400);
+  Serial.begin(9600);
   while (!Serial);
   Serial.println("AltIMU-10 V3 AHRS");
 
@@ -121,9 +99,6 @@ void loop()
   deltat = (now - last) * 1.0e-6; //seconds since last update
   last = now;
 
-  //for tests of acc/mag IGNORE GYRO for now
-  //  for (i = 0; i < 3; i++) Gxyz[i] = 0.0;
-
   MahonyQuaternionUpdate(Axyz[0], Axyz[1], Axyz[2], Gxyz[0], Gxyz[1], Gxyz[2],
                          Mxyz[0], Mxyz[1], Mxyz[2], deltat);
   // Define Tait-Bryan angles.
@@ -141,10 +116,14 @@ void loop()
   // pitch, and then roll.
   // http://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
   // which has additional links.
-  qw = q[0]; qx = q[1]; qy = q[2]; qz = q[3];
-  roll  = atan2(2.0 * (qw * qx + qy * qz), 1.0 - 2.0 * (qx * qx + qy * qy));
-  pitch = asin(2.0 * (qw * qy - qx * qz));
-  yaw   = atan2(2.0 * (qx * qy + qw * qz), 1.0 - 2.0 * ( qy * qy + qz * qz));
+//  qw = q[0]; qx = q[1]; qy = q[2]; qz = q[3];
+//  roll  = atan2(2.0 * (qw * qx + qy * qz), 1.0 - 2.0 * (qx * qx + qy * qy));
+//  pitch = asin(2.0 * (qw * qy - qx * qz));
+//  yaw   = atan2(2.0 * (qx * qy + qw * qz), 1.0 - 2.0 * ( qy * qy + qz * qz));
+  roll  = atan2((q[0] * q[1] + q[2] * q[3]), 0.5 - (q[1] * q[1] + q[2] * q[2]));
+  pitch = asin(2.0 * (q[0] * q[2] - q[1] * q[3]));
+  yaw   = atan2((q[1] * q[2] + q[0] * q[3]), 0.5 - ( q[2] * q[2] + q[3] * q[3]));
+
   // to degrees
   yaw   *= 180.0 / PI;
   pitch *= 180.0 / PI;
@@ -167,7 +146,7 @@ void loop()
     Serial.print(", ");
     Serial.println(roll, 0);
 //    Serial.print(", ");
-//    Serial.println(count); //number of loop iterations in this print interval (90 with 16 MHz Pro Mini)
+//    Serial.println(count); //number of loop iterations in this print interval (about 90 with 16 MHz Pro Mini)
     count=0;
   }
 }
@@ -203,127 +182,15 @@ void get_IMU_scaled(void) {
   Gxyz[0] = ((float) gyro.g.x - G_off[0]) * gscale; //d/s to radians/s
   Gxyz[1] = ((float) gyro.g.y - G_off[1]) * gscale;
   Gxyz[2] = ((float) gyro.g.z - G_off[2]) * gscale;
-  /*
-     // scaled data for debug
-     Serial.print(Axyz[0]);
-     Serial.print(", ");
-     Serial.print(Axyz[1]);
-     Serial.print(", ");
-     Serial.print(Axyz[2]);
-     Serial.print(", ");
-    Serial.print(Gxyz[0]);
-    Serial.print(", ");
-    Serial.print(Gxyz[1]);
-    Serial.print(", ");
-    Serial.print(Gxyz[2]);
-    Serial.print(", ");
-     Serial.print(Mxyz[0]);
-     Serial.print(", ");
-     Serial.print(Mxyz[1]);
-     Serial.print(", ");
-     Serial.print(Mxyz[2]);
-     Serial.println();
-     delay(500);
-  */
-}
-void MadgwickQuaternionUpdate(float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz, float deltat)
-{
-  // short name local variable for readability
-  float q1 = q[0], q2 = q[1], q3 = q[2], q4 = q[3];
-  float norm;
-  float hx, hy, _2bx, _2bz;
-  float s1, s2, s3, s4;
-  float qDot1, qDot2, qDot3, qDot4;
-
-  // Auxiliary variables to avoid repeated arithmetic
-  float _2q1mx;
-  float _2q1my;
-  float _2q1mz;
-  float _2q2mx;
-  float _4bx;
-  float _4bz;
-  float _2q1 = 2.0f * q1;
-  float _2q2 = 2.0f * q2;
-  float _2q3 = 2.0f * q3;
-  float _2q4 = 2.0f * q4;
-  float _2q1q3 = 2.0f * q1 * q3;
-  float _2q3q4 = 2.0f * q3 * q4;
-  float q1q1 = q1 * q1;
-  float q1q2 = q1 * q2;
-  float q1q3 = q1 * q3;
-  float q1q4 = q1 * q4;
-  float q2q2 = q2 * q2;
-  float q2q3 = q2 * q3;
-  float q2q4 = q2 * q4;
-  float q3q3 = q3 * q3;
-  float q3q4 = q3 * q4;
-  float q4q4 = q4 * q4;
-  /*  Already done in loop()
-    // Normalise accelerometer measurement
-    norm = sqrt(ax * ax + ay * ay + az * az);
-    if (norm == 0.0f) return; // handle NaN
-    norm = 1.0f / norm;
-    ax *= norm;
-    ay *= norm;
-    az *= norm;
-
-    // Normalise magnetometer measurement
-    norm = sqrt(mx * mx + my * my + mz * mz);
-    if (norm == 0.0f) return; // handle NaN
-    norm = 1.0f / norm;
-    mx *= norm;
-    my *= norm;
-    mz *= norm;
-  */
-  // Reference direction of Earth's magnetic field
-  _2q1mx = 2.0f * q1 * mx;
-  _2q1my = 2.0f * q1 * my;
-  _2q1mz = 2.0f * q1 * mz;
-  _2q2mx = 2.0f * q2 * mx;
-  hx = mx * q1q1 - _2q1my * q4 + _2q1mz * q3 + mx * q2q2 + _2q2 * my * q3 +
-       _2q2 * mz * q4 - mx * q3q3 - mx * q4q4;
-  hy = _2q1mx * q4 + my * q1q1 - _2q1mz * q2 + _2q2mx * q3 - my * q2q2 + my * q3q3 + _2q3 * mz * q4 - my * q4q4;
-  _2bx = sqrt(hx * hx + hy * hy);
-  _2bz = -_2q1mx * q3 + _2q1my * q2 + mz * q1q1 + _2q2mx * q4 - mz * q2q2 + _2q3 * my * q4 - mz * q3q3 + mz * q4q4;
-  _4bx = 2.0f * _2bx;
-  _4bz = 2.0f * _2bz;
-
-  // Gradient decent algorithm corrective step
-  s1 = -_2q3 * (2.0f * q2q4 - _2q1q3 - ax) + _2q2 * (2.0f * q1q2 + _2q3q4 - ay) - _2bz * q3 * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (-_2bx * q4 + _2bz * q2) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + _2bx * q3 * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
-  s2 = _2q4 * (2.0f * q2q4 - _2q1q3 - ax) + _2q1 * (2.0f * q1q2 + _2q3q4 - ay) - 4.0f * q2 * (1.0f - 2.0f * q2q2 - 2.0f * q3q3 - az) + _2bz * q4 * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (_2bx * q3 + _2bz * q1) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + (_2bx * q4 - _4bz * q2) * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
-  s3 = -_2q1 * (2.0f * q2q4 - _2q1q3 - ax) + _2q4 * (2.0f * q1q2 + _2q3q4 - ay) - 4.0f * q3 * (1.0f - 2.0f * q2q2 - 2.0f * q3q3 - az) + (-_4bx * q3 - _2bz * q1) * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (_2bx * q2 + _2bz * q4) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + (_2bx * q1 - _4bz * q3) * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
-  s4 = _2q2 * (2.0f * q2q4 - _2q1q3 - ax) + _2q3 * (2.0f * q1q2 + _2q3q4 - ay) + (-_4bx * q4 + _2bz * q2) * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (-_2bx * q1 + _2bz * q3) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + _2bx * q2 * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
-  norm = sqrt(s1 * s1 + s2 * s2 + s3 * s3 + s4 * s4);    // normalise step magnitude
-  norm = 1.0f / norm;
-  s1 *= norm;
-  s2 *= norm;
-  s3 *= norm;
-  s4 *= norm;
-
-  // Compute rate of change of quaternion
-  qDot1 = 0.5f * (-q2 * gx - q3 * gy - q4 * gz) - beta * s1;
-  qDot2 = 0.5f * (q1 * gx + q3 * gz - q4 * gy) - beta * s2;
-  qDot3 = 0.5f * (q1 * gy - q2 * gz + q4 * gx) - beta * s3;
-  qDot4 = 0.5f * (q1 * gz + q2 * gy - q3 * gx) - beta * s4;
-
-  // Integrate to yield quaternion
-  q1 += qDot1 * deltat;
-  q2 += qDot2 * deltat;
-  q3 += qDot3 * deltat;
-  q4 += qDot4 * deltat;
-  norm = sqrt(q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4);    // normalise quaternion
-  norm = 1.0f / norm;
-  q[0] = q1 * norm;
-  q[1] = q2 * norm;
-  q[2] = q3 * norm;
-  q[3] = q4 * norm;
-}
+  }
 
 // Mahony scheme uses proportional and integral filtering on
 // the error between estimated reference vectors and measured ones.
 void MahonyQuaternionUpdate(float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz, float deltat)
 {
-  // short name local variable for readability
+ // Vector to hold integral error for Mahony method
+  static float eInt[3] = {0.0, 0.0, 0.0};
+// short name local variable for readability
   float q1 = q[0], q2 = q[1], q3 = q[2], q4 = q[3];
   float norm;
   float hx, hy, bx, bz;
@@ -392,7 +259,7 @@ void MahonyQuaternionUpdate(float ax, float ay, float az, float gx, float gy, fl
     eInt[2] = 0.0f;
   }
 
-  // Apply feedback terms
+  // Apply feedback terms to gyro rate measurement
   gx = gx + Kp * ex + Ki * eInt[0];
   gy = gy + Kp * ey + Ki * eInt[1];
   gz = gz + Kp * ez + Ki * eInt[2];
